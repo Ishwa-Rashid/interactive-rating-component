2d3bd29a6aff0fa5629a5496c1698d893d23e4e4
Ishwa-Rashid <112717206+Ishwa-Rashid@users.noreply.github.com>
2023-07-29 10:34:23 -0700

    Updated

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const mainContainer = document.getElementById("container");
 const thanksContainer = document.getElementById("thank-you");
 const submitButton = document.getElementById("submit-rating");
 const rateAgain = document.getElementById("rate-again");
-const ratings = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn");
 const actualRating = document.getElementById("rating");
+let selected="";
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      
+        selected = e.target.textContent
+        e.target.classList.add("selected")
+    })
+})
 
 submitButton.addEventListener("click", () => {
   mainContainer.style.display = "none"
   thanksContainer.classList.remove("hidden")
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      actualRating.innerHTML = rating.innerHTML
-    })
-  })
+  actualRating.textContent = `${selected}`
+//   ratings.forEach((rating) => {
+//     rating.addEventListener("click", () => {
+//     actualRating.innerHTML = rating.innerHTML
+//     })
+//   })
 })
 
 rateAgain.addEventListener("click", () => {
